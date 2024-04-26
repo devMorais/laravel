dce99f6cc25e3d3e8938f67b39877ef14f1b7f91
Fernando Morais <fernando2015.aguiar@gmail.com>
2024-04-25 22:21:28 -0300

    Blade Directives

--- resources/views/blade-test.blade.php
+++ resources/views/blade-test.blade.php
@@ -1,6 +1,45 @@
 @extends('layouts.master')
+
 @section('content')
+@if (false)
+
 <h3>Conteúdo do site</h3>
+
+@elseif (true)
+
+<h3>Estou redenrizando se</h3>
+
+@else
+
+<h3>Estou redenrizando de outra parte</h3>
+@endif
+
+@php
+    $matriz = ['João', 'Antônio', 'Fernando', 'Miguel'];
+
+@endphp
+<ul>
+    @foreach ($matriz as $m )
+        <li>{{ $m }}</li>
+    @endforeach
+</ul>
+<ul>
+    @for ($i=0; $i < count($matriz); $i++ )
+        <li>{{ $matriz[$i] }}</li>
+    @endfor
+</ul>
+
+@empty()
+
+@endempty
+
+@include()
+@isset()
+
+@endisset
+
+
+
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
         Commodi soluta eius excepturi nesciunt perferendis optio nihil
         enim labore incidunt quo voluptatem quasi non, consequatur fugit vero?
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,10 +1,13 @@
 @extends('layouts.master')
 @section('content')
 
+
 <h3>Página de contato</h3>
+
 <p>
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi quas vero ipsa esse excepturi earum suscipit ducimus commodi eius
     qui natus magni, labore itaque provident, quam maiores, dolor illo facilis.
 </p>
 
 @endsection
+
